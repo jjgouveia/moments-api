1d08e72ea88e4705ed5615817abe9e9bdb8b82cb
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-04-27 14:52:50 -0300

    build: Iniciando o projeto

---
+++ src/main/java/com/api/moments/MomentsApplication.java
@@ -0,0 +1,13 @@
+package com.api.moments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/moments/MomentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.moments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
