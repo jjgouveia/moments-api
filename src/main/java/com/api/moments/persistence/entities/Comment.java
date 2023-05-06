37b9a151afd8ee286cd7eab47a4b5d071fab72c6
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-05-05 22:47:02 -0300

    feat: entidade inicial de comments

---
+++ src/main/java/com/api/moments/persistence/entities/Comment.java
@@ -0,0 +1,40 @@
+package com.api.moments.persistence.entities;
+
+import com.api.moments.util.Timestamp;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Document
+@Data
+public class Comment {
+  @Id
+  private UUID id;
+  @NotNull
+  private String content;
+  @NotNull
+  private UUID momentId;
+  @NotNull
+  private UUID userId;
+  private LocalDateTime createdAt;
+
+  public Comment(String content, UUID momentId, UUID userId) {
+    this.setId();
+    this.setContent(content);
+    this.setMomentId(momentId);
+    this.setUserId(userId);
+    this.setCreatedAt();
+  }
+
+  public void setId() {
+    this.id = UUID.randomUUID();
+  }
+
+  public void setCreatedAt() {
+    this.createdAt = Timestamp.getTimestamp();
+  }
+}
